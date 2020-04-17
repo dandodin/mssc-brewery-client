87fda1994420a0341dc0a4e951bbfa38fd7c9379
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-17 14:50:18 -0700

    moved values into prop file

--- src/main/java/guru/springframework/msscbreweryclient/web/ocnfig/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/ocnfig/BlockingRestTemplateCustomizer.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,6 +14,23 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnections;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(
+            @Value("${sfg.maxtotalconnections}") Integer maxTotalConnections,
+            @Value("${sfg.defaultmaxtotalconnections}") Integer defaultMaxTotalConnections,
+            @Value("${sfg.connectionrequesttimeout}") Integer connectionRequestTimeout,
+            @Value("${sfg.sockettimeout}") Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnections = defaultMaxTotalConnections;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
     @Override
     public void customize(RestTemplate restTemplate) {
         restTemplate.setRequestFactory(this.clientHttpRequestFactory());
@@ -23,13 +41,13 @@ public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
         Override with apache http client
          */
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnections);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
