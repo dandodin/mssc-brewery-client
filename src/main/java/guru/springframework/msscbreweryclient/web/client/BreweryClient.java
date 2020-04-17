e7f333c78f6c40664a83bf1193ad9ba6010116dd
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-17 13:41:51 -0700

    save changes

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -26,5 +27,17 @@ public class BreweryClient {
 
     public BeerDto getBeerById(UUID beerId) {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + beerId.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        restTemplate.put(apihost + BEER_PATH_V1 + beerId.toString(), beerDto);
+    }
+
+    public void deleteBeerById(UUID beerId) {
+        restTemplate.delete(apihost + BEER_PATH_V1 + beerId.toString());
     }
 }
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,5 +20,24 @@ class BreweryClientTest {
     void getBeerById() {
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        System.out.println(uri);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("Beer to update").build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeerById() {
+        breweryClient.deleteBeerById(UUID.randomUUID());
     }
 }
