24657f8bb0e067ac12462622d6b5849181dc24bb
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-17 13:57:21 -0700

    add customer calls

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,7 +24,7 @@ class BreweryClientTest {
     }
 
     @Test
-    void testSaveNewBeer() {
+    void saveNewBeer() {
         BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
         URI uri = breweryClient.saveNewBeer(beerDto);
         System.out.println(uri);
@@ -31,13 +32,38 @@ class BreweryClientTest {
     }
 
     @Test
-    void testUpdateBeer() {
+    void updateBeer() {
         BeerDto beerDto = BeerDto.builder().beerName("Beer to update").build();
         breweryClient.updateBeer(UUID.randomUUID(), beerDto);
     }
 
     @Test
-    void testDeleteBeerById() {
+    void deleteBeerById() {
         breweryClient.deleteBeerById(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        URI uri = breweryClient.saveNewCustomer(customerDto);
+        System.out.println(uri);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("Customer to update").build();
+        breweryClient.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomerById() {
+        breweryClient.deleteCustomerById(UUID.randomUUID());
     }
 }
